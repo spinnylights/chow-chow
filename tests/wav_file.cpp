d21ca5b08408167c686deed2cd5f2d2a0ea72a86
Zoë Sparks <zoe@milky.flowers>
2020-11-21 05:32:10 -0500

    Improve robustness of WAVFile conversion test
    Remove hardcoded values; write a routine that will work regardless of
    bit depth.

--- tests/wav_file.cpp
+++ tests/wav_file.cpp
@@ -1,10 +1,16 @@
 #include <doctest.h>
 
+#include <bitset>
+#include <climits>
+#include <cmath>
+
 #include "chow-chow/wav_file.hpp"
 
 using namespace ChowChow;
 
-WAVFile file() { return WAVFile {{1., 0.5, -1., -0.5}, 48000}; }
+static const std::vector<long double> expected = {1., 0.5, -1., -0.5};
+
+WAVFile file() { return WAVFile {expected, 48000}; }
 
 TEST_CASE("length of header") {
     CHECK(file().header().length() == 44);
@@ -12,12 +18,34 @@ TEST_CASE("length of header") {
 
 TEST_CASE("data conversion") {
     auto data = file().data();
-    CHECK(data[0] == -1);
-    CHECK(data[1] == 127);
-    CHECK(data[2] == -1);
-    CHECK(data[3] == 63);
-    CHECK(data[4] == 1);
-    CHECK(data[5] == -128);
-    CHECK(data[6] == 1);
-    CHECK(data[7] == -64);
+    for (std::size_t i = 0; i < data.length(); i += WAVFile::BYTES_PER_SAMPLE) {
+        std::bitset<WAVFile::BITS_PER_SAMPLE> sample = {};
+
+        for (std::size_t j = 0; j < WAVFile::BITS_PER_SAMPLE / CHAR_BIT; ++j) {
+            std::bitset<CHAR_BIT> bs = data[i+j];
+            for (std::size_t k = 0; k < CHAR_BIT; ++k) {
+                sample[k + CHAR_BIT*j] = bs[k];
+            }
+        }
+
+        std::bitset<32> sample_n = {};
+        for (std::size_t j = 0; j < sample.size() - 1; ++j) {
+            sample_n[j] = sample[j];
+        }
+        sample_n[sample_n.size() - 1] = sample[sample.size() - 1];
+
+        if (sample_n[sample_n.size() - 1] == 1) {
+            for (std::size_t j = 0; j < 8; ++j) {
+                sample_n[sample_n.size() - 9 + j] = 1;
+            }
+        }
+
+        int32_t sample_i = sample_n.to_ulong();
+
+        double sample_f =
+            static_cast<double>(sample_i)
+            / (std::pow(2, WAVFile::BITS_PER_SAMPLE - 1) - 1);
+
+        CHECK(expected[i / WAVFile::BYTES_PER_SAMPLE] - sample_f < 1.e-7);
+    }
 }
