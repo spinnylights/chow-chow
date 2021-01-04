7e2c6c059b96a518186bf64994974a9864a3e72d
Zoë Sparks <zoe@milky.flowers>
2021-01-04 05:02:17 -0500

    Clean up commented-out code in Operator impl

--- src/operator.cpp
+++ src/operator.cpp
@@ -8,27 +8,17 @@ using namespace ChowChow;
 
 double Operator::sig(double mod) const
 {
-    //if (vibr_on) {
-    //    return phase_acc.amp(vibr.amp() + mod);
-    //} else {
-        return phase_acc.amp(mod);
-    //}
+    return phase_acc.amp(mod);
 }
 
 double Operator::sig() const
 {
-    //if (vibr_on) {
-    //    return phase_acc.amp(vibr.amp());
-    //} else {
-        return phase_acc.amp();
-    //}
+    return phase_acc.amp();
 }
 
 void Operator::advance()
 {
     if (vibr_on) {
-        //const auto frq_base = (frq + vibr.amp()) * rtio;
-        //phase_acc.frequency(frq_base + frq_offset);
         phase_acc.advance(vibr);
         vibr.advance();
     } else {
@@ -50,7 +40,6 @@ void Operator::freq(double n)
 void Operator::freq_offset(double n)
 {
     frq_offset = n * 0.001;
-    //frq_offset = n * 0.1;
     reset_frq();
 }
 
